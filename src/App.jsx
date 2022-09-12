f555bddfbab394e97741732d2cd00c9445d4f6e3
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-09-12 17:35:21 +0400

    state for handling change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import "./assets/css/style.css";
 
 function App() {
+	const [text, setText] = useState("");
+
+	function handleChange(event) {
+		const { value } = event.target;
+
+		setText(value);
+	}
+	console.log(text)
 	return (
 		<div className="container">
 			<div className="main">
@@ -9,12 +17,13 @@ function App() {
 					<h1>How fast can you type?</h1>
 				</div>
 				<div className="game">
-					<textarea name="text"></textarea>
+					<textarea value={text} onChange={handleChange} />
 				</div>
 
 				<div className="submission">
 					<h3>Time Remaining: 5</h3>
 					<button className="submit">Start</button>
+					<h4>Word Count: 10</h4>
 				</div>
 			</div>
 		</div>
